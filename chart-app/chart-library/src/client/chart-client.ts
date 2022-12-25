4e9a435468c0d2b6bd6b4470d70fddd7ab4d3d93
Hadeel <hadeelalomary66@gmail.com>
2022-12-25 12:10:08 +0200

    update code state

--- chart-app/chart-library/src/client/chart-client.ts
+++ chart-app/chart-library/src/client/chart-client.ts
@@ -1,5 +1,10 @@
 
 export class ChartClient {
+
+  constructor() {
+    console.log("--------------- Library -----------------")
+  }
+
   //
   // chart:Chart = null;
   //
--- chart-app/src/app/app.component.ts
+++ chart-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ChartClient} from "../../chart-library/src/client";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chart-app';
+
+  constructor() {
+    new ChartClient();
+  }
 }
